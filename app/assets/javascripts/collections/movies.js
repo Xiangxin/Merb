5993c86e7a2eb41169cd6bbc3ddfeae144aebd12
pzw2041 <pzw2041@gmail.com>
2013-11-11 16:32:21 +0800

    fix pagination

--- app/assets/javascripts/collections/movies.js
+++ app/assets/javascripts/collections/movies.js
@@ -3,10 +3,10 @@ var Movies = Merb.Collection.extend({
 
 	pagination : function(page) {
 		this.url = url + page.toString();
-       	page = page-1;
+       	//page = page-1;
        	var collection = this;
-       	collection = _(collection.rest(perPage * page));
-       	collection = _(collection.first(perPage));    
+       	//collection = _(collection.rest(perPage * page));
+       	//collection = _(collection.first(perPage));    
        	return collection.map( function(model) { return model.toJSON() } ); 
 	}
 });
--- app/assets/javascripts/views/app_view.js
+++ app/assets/javascripts/views/app_view.js
@@ -4,9 +4,10 @@ var AppView = Merb.View.extend({
 	//el: set element of main div changing
 
 	showPage: function(pageNum){
+       //movies.url = movies.url + "?" + $.param({page: pageNum});
+
         $('#pagination').show();
         $("div.row-movies").empty();
-       //movies.url = movies.url + "?" + $.param({page: pageNum});
 
         movies.fetch({
             //using jquery param method to add param to url
@@ -16,6 +17,7 @@ var AppView = Merb.View.extend({
               
             //upon success, run function
             success : function(data){
+
                 // movies.reset(data.models)
                 movies_view = new MoviesView({ }) //create collection view
                 _.each(movies.models, function(model){ //for each movie model in the collection, pass in that model
@@ -32,6 +34,7 @@ var AppView = Merb.View.extend({
                 //set the html elements
                 $('#prevPage').attr("href", "/#page/"+prev_page_num);
                 $('#nextPage').attr("href", "/#page/"+next_page_num);
+
             },
 
             error: function(error){
